dac8b6775149a48501825a61064c5f9cc64fa063
Harris <Nathan.Harris@adesa.com>
2020-06-15 13:03:19 -0400

    Revert "Added eslint, prettier, and tsconfig files"
    This reverts commit f9e628877dece439125740a192978e7fe68aac0b.

--- design-patterns/adapter/adapter.ts
+++ design-patterns/adapter/adapter.ts
@@ -1,81 +1,84 @@
 class LegacyData {
-    id: number;
-    action: string;
-    enabled: number;
+  id: number;
+  action: string;
+  enabled: number;
 
-    constructor(id: number, action: string, enabled: number) {
-        this.id = id;
-        this.action = action;
-        this.enabled = enabled;
-    }
+  constructor(id: number, action: string, enabled: number) {
+    this.id = id;
+    this.action = action;
+    this.enabled = enabled;
+  }
 }
 
 class LegacySystem {
-    processData(data: LegacyData): LegacyData {
-        console.log(`Legacy system: Processing ${JSON.stringify(data)}`);
-        if (data.action === 'Create') {
-            return new LegacyData(12345, 'New', 1);
-        } else {
-            return new LegacyData(data.id, 'Updated', data.enabled);
-        }
+  processData(data: LegacyData): LegacyData {
+    console.log(`Legacy system: Processing ${JSON.stringify(data)}`);
+    if (data.action === "Create") {
+      return new LegacyData(12345, "New", 1);
+    } else {
+      return new LegacyData(data.id, "Updated", data.enabled);
     }
+  }
 }
 
 interface DataCommand {
-    getAccount(): string;
-    getAction(): string;
-    isEnabled(): boolean;
+  getAccount(): string;
+  getAction(): string;
+  isEnabled(): boolean;
 }
 
 class CreateDataCommand implements DataCommand {
-    account: string;
-    enabled: boolean;
+  account: string;
+  enabled: boolean;
 
-    constructor(account: string, enabled: boolean) {
-        this.account = account;
-        this.enabled = enabled;
-    }
+  constructor(account: string, enabled: boolean) {
+    this.account = account;
+    this.enabled = enabled;
+  }
 
-    getAccount(): string {
-        return this.account;
-    }
-    getAction(): string {
-        return 'Create';
-    }
-    isEnabled(): boolean {
-        return this.enabled;
-    }
+  getAccount(): string {
+    return this.account;
+  }
+  getAction(): string {
+    return "Create";
+  }
+  isEnabled(): boolean {
+    return this.enabled;
+  }
 }
 
 class LegacyAdapter {
-    private legacySystem: LegacySystem;
+  private legacySystem: LegacySystem;
 
-    constructor(legacySystem: LegacySystem) {
-        this.legacySystem = legacySystem;
-    }
+  constructor(legacySystem: LegacySystem) {
+    this.legacySystem = legacySystem;
+  }
 
-    processData(dataCommand: DataCommand): DataCommand {
-        const input = this.convertCommandToLegacyData(dataCommand);
-        const output = this.legacySystem.processData(input);
-        return this.convertLegacyToCommandData(output);
-    }
+  processData(dataCommand: DataCommand): DataCommand {
+    const input = this.convertCommandToLegacyData(dataCommand);
+    const output = this.legacySystem.processData(input);
+    return this.convertLegacyToCommandData(output);
+  }
 
-    private convertCommandToLegacyData(dataCommand: DataCommand): LegacyData {
-        return new LegacyData(
-            parseInt(dataCommand.getAccount()),
-            dataCommand.getAction(),
-            dataCommand.isEnabled() ? 1 : 0
-        );
-    }
+  private convertCommandToLegacyData(dataCommand: DataCommand): LegacyData {
+    return new LegacyData(
+      parseInt(dataCommand.getAccount()),
+      dataCommand.getAction(),
+      dataCommand.isEnabled() ? 1 : 0
+    );
+  }
 
-    private convertLegacyToCommandData(legacyData: LegacyData): DataCommand {
-        return new CreateDataCommand(legacyData.id.toString(), legacyData.enabled === 1 ? true : false);
-    }
+  private convertLegacyToCommandData(legacyData: LegacyData): DataCommand {
+    return new CreateDataCommand(
+      legacyData.id.toString(),
+      legacyData.enabled === 1 ? true : false
+    );
+  }
 }
 
 const legacySystem = new LegacySystem();
 const legacyAdapter = new LegacyAdapter(legacySystem);
-const myCommand = new CreateDataCommand('0', false);
+const myCommand = new CreateDataCommand("0", false);
 
 console.log(`Creating new account for ${JSON.stringify(myCommand)}`);
 
--- design-patterns/builder/builder.ts
+++ design-patterns/builder/builder.ts
@@ -1,60 +1,60 @@
 interface Car {
-    make: string;
-    model: string;
-    color: string;
-    vin: string;
+  make: string;
+  model: string;
+  color: string;
+  vin: string;
 }
 
 class CarBuilder {
-    private readonly car: Car;
+  private readonly car: Car;
 
-    constructor(make: string, model: string) {
-        if (make == null) throw Error('Make cannot be null.');
-        if (model == null) throw Error('Model cannot be null.');
+  constructor(make: string, model: string) {
+    if (make == null) throw Error("Make cannot be null.");
+    if (model == null) throw Error("Model cannot be null.");
 
-        this.car = {
-            make: make,
-            model: model,
-            color: '',
-            vin: '',
-        };
-    }
+    this.car = {
+      make: make,
+      model: model,
+      color: "",
+      vin: "",
+    };
+  }
 
-    color(color: string): CarBuilder {
-        if (color == null) throw Error('Color cannot be null.');
-        this.car.color = color;
-        return this;
-    }
+  color(color: string): CarBuilder {
+    if (color == null) throw Error("Color cannot be null.");
+    this.car.color = color;
+    return this;
+  }
 
-    vin(vin: string): CarBuilder {
-        if (vin == null) throw Error('Vin cannot be null.');
-        this.car.vin = vin;
-        return this;
-    }
+  vin(vin: string): CarBuilder {
+    if (vin == null) throw Error("Vin cannot be null.");
+    this.car.vin = vin;
+    return this;
+  }
 
-    build(): Car {
-        return this.car;
-    }
+  build(): Car {
+    return this.car;
+  }
 }
 
-let myCar = new CarBuilder('Honda', 'Accord').build();
+let myCar = new CarBuilder("Honda", "Accord").build();
 console.log(`myCar: ${JSON.stringify(myCar)}\n`);
 
-myCar = new CarBuilder('Chevrolet', 'Silverado').color('Blue').build();
+myCar = new CarBuilder("Chevrolet", "Silverado").color("Blue").build();
 console.log(`myCar: ${JSON.stringify(myCar)}\n`);
 
-myCar = new CarBuilder('Toyota', 'Camry').vin('WMWXS5C56FT892627').build();
+myCar = new CarBuilder("Toyota", "Camry").vin("WMWXS5C56FT892627").build();
 console.log(`myCar: ${JSON.stringify(myCar)}\n`);
 
-const myCarBuilder = new CarBuilder('Toyota', 'Camry');
+const myCarBuilder = new CarBuilder("Toyota", "Camry");
 console.log(`myCarBuilder: ${JSON.stringify(myCarBuilder)}`);
 
 console.log(`Retrieving VIN from repo....`);
-myCarBuilder.vin('5NPE34AB4FH138219');
+myCarBuilder.vin("5NPE34AB4FH138219");
 console.log(`myCarBuilder: ${JSON.stringify(myCarBuilder)}`);
 
-console.log('Retrieving Color from API....');
-myCarBuilder.color('Red');
+console.log("Retrieving Color from API....");
+myCarBuilder.color("Red");
 console.log(`myCarBuilder: ${JSON.stringify(myCarBuilder)}`);
 
 console.log(`myCar: ${JSON.stringify(myCarBuilder.build())}`);
